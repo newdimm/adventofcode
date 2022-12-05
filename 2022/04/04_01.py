b0e47c450919d899e3d91d9cbcc5cfdeeecc0738
Dmitrii Vasilchenko <dmitriiv@qti.qualcomm.com>
2022-12-05 21:58:02 +0000

    Day 4

---
+++ 2022/04/04_01.py
@@ -0,0 +1,15 @@
+f = open("input.txt")
+
+counter = 0
+
+for line in f:
+    pair = line.strip().split(",")
+    range1 = [int(x) for x in pair[0].split("-")]
+    range2 = [int(x) for x in pair[1].split("-")]
+    
+    #print("%s :: %s" % (range1, range2))
+    if range1[0] <= range2[0] and range1[1] >= range2[1] \
+        or range2[0] <= range1[0] and range2[1] >= range1[1]:
+        counter += 1
+        
+print(counter)
